0be80f45f6331778f5546c716d000795fe937844
douglas conley <douglas.conley@gmail.com>
2022-09-25 22:13:54 -0300

    hhhg

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+
+Repositorio, IA-4 para solciones financieras
+
+*/
+
+$html = "";
+
+$head = "";
+
+$html .= $head;
+
+$body = "";
+
+$html .= $body;
+
+$foot = "";
+
+$html .= $foot;
+
+echo $html;
+
+?>
